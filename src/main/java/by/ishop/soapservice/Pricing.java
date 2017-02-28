a8c6aee36275b384c0e8b70f6203aed9c4103c2f
Stenukhin Alexey <aasten@gmail.com>
2017-02-28 18:04:59 +0300

    Interface segregation.

--- src/main/java/by/ishop/soapservice/Pricing.java
+++ src/main/java/by/ishop/soapservice/Pricing.java
@@ -3,5 +3,4 @@ package by.ishop.soapservice;
 public interface Pricing {
 
     EntryPrice getEntryPrice(String entryId) throws NoEntryPricingInfo;
-
 }
--- src/main/java/by/ishop/soapservice/PricingEndpoint.java
+++ src/main/java/by/ishop/soapservice/PricingEndpoint.java
@@ -5,25 +5,29 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
+import java.math.BigDecimal;
 
 @WebService(serviceName = "PricingEndpoint")
 @SOAPBinding(style = Style.RPC)
-public class PricingEndpoint {
-    
-    private Pricing pricing = new TestPricing();
-    
-    public PricingEndpoint(Pricing pricing) {
-        this.pricing = pricing;
-    }
+public class PricingEndpoint implements Pricing {
     
     public PricingEndpoint() {} // required for jax rs 1.0
     
+    @Override
     @WebMethod
-    public EntryPrice getEntryPrice(@WebParam(name="entryId") String entryId) throws NoEntryPricingInfo{
-        return pricing.getEntryPrice(entryId);
+    public EntryPrice getEntryPrice(@WebParam(name="entryId") String entryId) throws NoEntryPricingInfo {
+        Long entryNumId;
+        if(null == entryId) throw new NoEntryPricingInfo("Argument is null");
+        try {
+            entryNumId = Long.parseLong(entryId);
+            if(entryNumId < 0) throw new RuntimeException("Entry id must be positive integer");
+        } catch(RuntimeException e) {
+            throw new NoEntryPricingInfo("Bad entry id", e);
+        }
+        // stub
+        EntryPrice stub = new EntryPrice();
+        stub.setCurrencyCode("USD");
+        stub.setValue(new BigDecimal("42"));
+        return stub;
     }
-    
-//    public static void main(String[] args) {
-//        Endpoint.publish("http://localhost:7000/soapservice", new PricingEndpoint(new TestPricing()));
-//     }
 }
